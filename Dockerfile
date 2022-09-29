fc9b9152dd2bacfe1a7e3335d51ca8faea257b95
Denis Balan <denis.balan@endava.com>
2022-09-29 09:00:21 +0300

    Modify index.html, add vim

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM nginx:1.23.1
 COPY index.html /usr/share/nginx/html
+RUN apt-get update && apt install -y vim
--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
+hostname=$(hostname)
+sed -i s/HSTNME/$hostname/g index.html
 docker build . -t nginx_web
